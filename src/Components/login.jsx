5ea6bb9a72107f043e4a310c7e1264a092cbd265
Willy kalisa <127384315+Kalisa21@users.noreply.github.com>
2024-08-02 12:04:11 +0000

    dash

--- src/Components/login.jsx
+++ src/Components/login.jsx
@@ -1,14 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
   const navigate = useNavigate();
+  const [role, setRole] = useState('user');
 
   const handleSubmit = (event) => {
     event.preventDefault();
     // Handle form submission logic here
-    // After successful login, navigate to the dashboard
-    navigate('/home');
+    // After successful login, navigate to the appropriate dashboard based on the role
+    if (role === 'user') {
+      navigate('/user-dashboard');
+    } else {
+      navigate('/employer-dashboard');
+    }
   };
 
   return (
@@ -21,6 +26,13 @@ const Login = () => {
           <input type="email" id="email" name="email" className="w-full p-2 mb-5 border border-gray-300 rounded" required />
           <label htmlFor="password" className="block text-left text-gray-500 mb-1">Password</label>
           <input type="password" id="password" name="password" className="w-full p-2 mb-5 border border-gray-300 rounded" required />
+          
+          <label htmlFor="role" className="block text-left text-gray-500 mb-1">Login as:</label>
+          <select id="role" name="role" className="w-full p-2 mb-5 border border-gray-300 rounded" value={role} onChange={(e) => setRole(e.target.value)}>
+            <option value="user">User</option>
+            <option value="employer">Employer</option>
+          </select>
+
           <input type="submit" value="Submit" className="w-full p-2 bg-blue-700 text-white font-bold rounded-3xl cursor-pointer hover:bg-green-700" />
         </form>
         <div className="mt-5">
